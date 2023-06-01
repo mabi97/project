2da603bb39cdf3b8ef9684f4cbf56747d84cd543
mabi97 <manhtd97@gmail.com>
2023-06-01 22:26:59 +0700

    update

--- shopee/query.sql
+++ shopee/query.sql
@@ -1,2 +1,19 @@
-SELECT *
-FROM item
+CREATE TABLE items (
+    id TEXT PRIMARY KEY,
+    shop_id TEXT, 
+    name TEXT,
+    stock INTEGER,
+    sold INTEGER,
+    liked INTEGER,
+    comment INTEGER,
+    status TEXT,
+    price INTEGER, 
+    price_min INTEGER,
+    price_max INTEGER,
+    discount INTEGER,
+    rating_1 INTEGER,
+    rating_2 INTEGER,
+    rating_3 INTEGER,
+    rating_4 INTEGER,
+    rating_5 INTEGER
+)
