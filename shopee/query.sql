f9b67fee8805ae9d5ea54d9ed6487a107a800558
mabi97 <manhtd97@gmail.com>
2023-06-12 21:47:37 +0700

    update

--- shopee/query.sql
+++ shopee/query.sql
@@ -1,19 +1,2 @@
-CREATE TABLE items (
-    id TEXT PRIMARY KEY,
-    shop_id TEXT, 
-    name TEXT,
-    stock INTEGER,
-    sold INTEGER,
-    liked INTEGER,
-    comment INTEGER,
-    status TEXT,
-    price INTEGER, 
-    price_min INTEGER,
-    price_max INTEGER,
-    discount INTEGER,
-    rating_1 INTEGER,
-    rating_2 INTEGER,
-    rating_3 INTEGER,
-    rating_4 INTEGER,
-    rating_5 INTEGER
-)
+SELECT COUNT(id)
+FROM shop
